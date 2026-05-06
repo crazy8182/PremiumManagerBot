79b4de992946ec0a6348e2b04743537ce36a59e2
crazy8182 <crazygaming4694@gmail.com>
2026-05-06 16:44:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,8 +16,8 @@ UPI_ID = "yourname@upi"
 QR_IMAGE = "assets/qr.png"
 
 PLANS = {
-    "1month": {"days": 30, "price": "99", "name": "1 Month"},
-    "3month": {"days": 90, "price": "249", "name": "3 Month"},
-    "6month": {"days": 180, "price": "449", "name": "6 Month"},
-    "12month": {"days": 365, "price": "799", "name": "1 Year"}
+    "1month": {"days": 30, "price": "99", "name": "Monthly"},
+    "3month": {"days": 90, "price": "249", "name": "Quarterly"},
+    "6month": {"days": 180, "price": "449", "name": "Haf-Yearly"},
+    "12month": {"days": 365, "price": "799", "name": "Yearly"}
 }
